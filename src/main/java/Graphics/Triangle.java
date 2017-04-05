00776927bb81b998b66c07aebedc072f7989a81f
Alex_V <vilenskii1997@mail.ru>
2017-04-05 22:13:41 +0300

    Check using max variable.

--- src/main/java/Graphics/Triangle.java
+++ src/main/java/Graphics/Triangle.java
@@ -1,17 +1,23 @@
 package Graphics;
 
 
-public class Graphics{
+public class Triangle {
     public static void stars (int count)
     {
-        for(int i=0;i<count;++i) {
-            for (int j = 0; j < i; ++j)
-                System.out.print("h");
-                System.out.println();
 
+
+        //создали вложенный цикл, в котором будет выводится треугольник
+        for(int i=0; i<count; ++i) {
+            for (int j = 0; j < i; ++j)
+                System.out.print("*");
+                System.out.println();
         }
     }
     public static void main(String[] args) {
+        int x = 5;
      stars(20);
+        for (int i = 0; i < x; ++i) {
+            System.out.println(i);
+        }
     }
 }
